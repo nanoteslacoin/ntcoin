e1862414983c7fc38bb45f6be8526cd2d889a433
root <root@s132-148-82-213.secureserver.net>
2016-12-26 05:55:32 -0700

    add1

---
+++ src/qt/transactiondescdialog.cpp
@@ -0,0 +1,28 @@
+#include "transactiondescdialog.h"
+#include "ui_transactiondescdialog.h"
+
+#include "transactiontablemodel.h"
+
+#include <QModelIndex>
+#include <QPushButton>
+
+TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TransactionDescDialog)
+{
+    ui->setupUi(this);
+    ui->buttonBox->button(QDialogButtonBox::Close)->setText(QString("关闭"));
+
+    QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
+    ui->detailText->setHtml(desc);
+}
+
+TransactionDescDialog::~TransactionDescDialog()
+{
+    delete ui;
+}
+
+void TransactionDescDialog::on_pushButton1_clicked(bool checked)
+{
+
+}
